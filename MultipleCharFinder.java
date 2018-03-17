d6ed16fe46a24b2d96fbf57dd3cc53fa1e0719bc
Elvin-Arrow <30791291+Elvin-Arrow@users.noreply.github.com>
2018-03-17 19:28:08 +0500

    15/3/18

---
+++ MultipleCharFinder.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class MultipleCharFinder{
+  public static void main(String[] args){
+    //Declaration block
+    Scanner input = new Scanner(System.in);
+    int j, foundCount, strLength, searchLength, index;
+    String str, searchItem;
+    //Initialization block
+    j = 0;
+    foundCount = 0;
+    index = 0;
+    //Working block
+    str = input.nextLine();
+    strLength = str.length();
+    searchItem = input.nextLine();
+    searchLength = searchItem.length();
+    for (int i = 0; i < strLength; i++){
+      if (searchItem.charAt(j) == str.charAt(i)){
+        if (j == 0){
+          index = i;
+        }
+        j++;
+      }else{
+        j = 0;
+      }
+      if (j == searchLength){
+        System.out.println("Item found at index: " + index);
+        j = 0;
+        foundCount++;
+      }
+    }
+    if (j==0){
+      System.out.println("Search Item " + searchItem + " found at " + foundCount + " places");
+    }else{
+      System.out.println("Item not found");
+    }
+  }
+}
