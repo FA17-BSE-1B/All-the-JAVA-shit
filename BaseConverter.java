7890d89036a9e9163023442cc7e989ac3fd6bcb7
Elvin-Arrow <30791291+Elvin-Arrow@users.noreply.github.com>
2018-04-07 22:10:56 +0500

    22/02/18

--- BaseConverter.java
+++ BaseConverter.java
@@ -2,14 +2,11 @@ import java.util.Scanner;
 
 public class BaseConverter{
   public static void main(String[] args){
-    //Declaration block
     String userInput;
     int base;
     boolean validity;
     Scanner input;
-    //Initialization block
     input = new Scanner(System.in);
-    //Working block
     System.out.println();
     System.out.println("Enter the number along with the base");
     System.out.print("Number: ");
@@ -34,13 +31,10 @@ public class BaseConverter{
 
   }
   public static boolean validateNumber(String stringNum, int base){
-    //Declaration block
     int numLegnth;
     boolean validFlag;
-    //Initialization block
     numLegnth = stringNum.length();
     validFlag = false;
-    //Working block
     System.out.println();
     if (base == 2){
       for (int i = numLegnth-1; i >= 0; i--){
@@ -113,12 +107,8 @@ public class BaseConverter{
     //Declaration block
     String binaryNum, binary;
     int len;
-
-    //Initialization block
     binary = "";
     binaryNum = "";
-
-    //Working block
     while (decimalNum >= 1){
       binary += decimalNum % 2;
       decimalNum /= 2;
@@ -131,14 +121,11 @@ public class BaseConverter{
   }
 
   public static String decimalToOctal(int decimalNum){
-    //Declaration block
     int len;
     String octalNumber, num;
-    //Initialization block
     octalNumber = "";
     num = "";
     len = 0;
-    //Working block
     while (decimalNum >= 1){
       num += (decimalNum % 8);
       decimalNum /= 8;
@@ -151,15 +138,10 @@ public class BaseConverter{
   }
 
   public static String decimalToHex(int decimalNum){
-    //Declaration block
     int remainder, len;
     String hexNumber, hex;
-
-    //Initialization block
     hex = "";
     hexNumber = "";
-
-    //Working block
     while (decimalNum >= 16){
       remainder = decimalNum % 16;
       if (remainder > 9){
@@ -182,27 +164,18 @@ public class BaseConverter{
   }
 
   public static void binaryToOther(String num){
-    //Declaration block
     int numLength, decimalNum, power;
     String octalNum, hexNum;
-
-    //Initialization block
     decimalNum = 0;
     power = 0;
     numLength = num.length();
-
-    //Working block
-    //Binary to decimal
     for (int i = numLength-1; i >= 0; i--){
       decimalNum += (num.charAt(i)- '0') * (int)Math.pow(2, power);
       power++;
     }
-    //Decimal to Octal
     octalNum = decimalToOctal(decimalNum);
-    //Decimal to hex
     hexNum = decimalToHex(decimalNum);
 
-    //Priting out the obtained values
     System.out.println("Following are all the conversions for the binary number: " + num);
     System.out.println("==========================================================");
     System.out.println("Decimal: " + decimalNum);
@@ -213,26 +186,18 @@ public class BaseConverter{
   }
 
   public static void octalToOther(String num){
-    //Declaration block
     String binaryNum, hexNum;
     int decimalNum;
     short numLength, exponent;
-    //Initialization block
     decimalNum = 0;
     exponent = 0;
     numLength = (short)num.length();
-    //Working block
-    //Octal to decimal
     for (int i = numLength-1; i >= 0; i--){
       decimalNum += (num.charAt(i) - '0') * (int)(Math.pow(8, exponent));
       exponent++;
       }
-      //Decimal to binary
     binaryNum = decimalToBinary(decimalNum)  ;
-    //Decimal to hex
     hexNum = decimalToHex(decimalNum);
-
-    //Display the converted values
     System.out.println("Following are all the conversions for the octal number: " + num);
     System.out.println("==========================================================");
     System.out.println("Binary: " + binaryNum);
@@ -243,21 +208,12 @@ public class BaseConverter{
   }
 
   public static void decimalToOther(String num){
-    //Declaration block
     String binaryNum, octalNum, hexNum;
     int decimalNum;
-    //Initialization block
-
-    //Working block
     decimalNum = Integer.parseInt(num);
-    //Decimal to binary
     binaryNum = decimalToBinary(decimalNum);
-    //Decimal to octal
     octalNum = decimalToOctal(decimalNum);
-    //Decimal to hex
     hexNum = decimalToHex(decimalNum);
-
-    //Display the obtained results
     System.out.println("Following are all the conversions for the decimal number: " + num);
     System.out.println("==========================================================");
     System.out.println("Binary: " + binaryNum);
@@ -266,18 +222,12 @@ public class BaseConverter{
     System.out.println("==========================================================");
     System.out.println();
   }
-
   public static void hexToOther(String num){
-    //Declaration block
     int numLength, exponent, decimalNum, digit;
     String binaryNum, octalNum;
-    //Initialization block
     numLength = num.length();
     decimalNum = 0;
     exponent = 0;
-
-    //Working block
-    //Hex to decimal
     for (int i = numLength-1; i >= 0; i--){
         digit = (char)num.charAt(i);
         if ((char)digit >= 'A'){
@@ -288,13 +238,8 @@ public class BaseConverter{
         decimalNum += (digit * (int)Math.pow(16, exponent));
         exponent++;
     }
-
-    // Decimal to binary
     binaryNum = decimalToBinary(decimalNum);
-    //Decimal to octal
     octalNum = decimalToOctal(decimalNum);
-
-    //Display the obtained results
     System.out.println("Following are all the conversions for the decimal number: " + num);
     System.out.println("==========================================================");
     System.out.println("Binary: " + binaryNum);
