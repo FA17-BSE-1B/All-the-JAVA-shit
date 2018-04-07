3dc829c48539a393c510c6b1c0f6682acb077256
Elvin-Arrow <30791291+Elvin-Arrow@users.noreply.github.com>
2018-04-07 22:12:13 +0500

    15/02/18

--- DigitSeparator.java
+++ DigitSeparator.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
 
 public class DigitSeparator{
-  public static void main(String args[]){ // Main method
-    Scanner input = new Scanner(System.in); //Object creation to take input from the user
+  public static void main(String args[]){ 
+    Scanner input = new Scanner(System.in);
     int num, count, i;
     System.out.print("Enter the number to be seperated: ");
-    num = input.nextInt(); //Get the number from the user in the variable num
-    count = DigCount(num); //Call the function DigCount and store the returning value in variable count
-    int [] digit = new int[count]; //Initiate an array of the size of number of digits
-    i = count - 1; //Index starts from zero, -1 to adjust the value to entertain array size
+    num = input.nextInt(); 
+    count = DigCount(num); 
+    int [] digit = new int[count];
+    i = count - 1; 
     while (i >= 0){
-      digit[i] = Seperator(num);  //Call the separator method and get the digit
-      num -= digit[i]; // Remove the extracted digit from the number
-      num /= 10; // Remove the zero to make the number ready for next separation
-      i -= 1; //Adjust the value of loop controlling variable for next iteration
+      digit[i] = Seperator(num);
+      num -= digit[i]; 
+      num /= 10;
+      i -= 1;
     }
-    for (int x: digit){ //For each loop in java is written like this
+    for (int x: digit){ 
       System.out.println(x);
     }
   }
