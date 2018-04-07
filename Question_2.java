75cfa6629294e15938712be2b88f9cd98a7827be
Elvin-Arrow <30791291+Elvin-Arrow@users.noreply.github.com>
2018-04-07 22:14:42 +0500

    Lab Sessional 1

--- Question_2.java
+++ Question_2.java
@@ -1,17 +1,14 @@
 import java.util.Scanner;
 public class Question_2{
   public static void main(String[] args){
-    //Declaration block
     Scanner getInput = new Scanner(System.in);
     int num, reverse, exponent;
     String temp;
-    //Initialization block
     reverse = 0;
-    //Working block
     System.out.print("Enter the number: ");
     num = getInput.nextInt();
-    temp = num + ""; //Convert number to a string
-    exponent = temp.length() - 1; //Obtain the length of the string number
+    temp = num + ""; 
+    exponent = temp.length() - 1; 
     while (num > 0){
       reverse += (num % 10) * ((int)Math.pow(10, exponent));
       num /= 10;
