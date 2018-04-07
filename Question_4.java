f0a6fa2548ae7a7308925e2183925cb8e88c6273
Elvin-Arrow <30791291+Elvin-Arrow@users.noreply.github.com>
2018-04-07 22:16:16 +0500

    Lab sessional 1

--- Question_4.java
+++ Question_4.java
@@ -2,23 +2,20 @@ import java.util.Scanner;
 
 public class Question_4{
   public static void main(String[] args){
-    //Declaration block
     Scanner input;
     Boolean found;
     String str, item;
     int i, j, item_len, start, end;
-    //Initialization block
     found = false;
     j = 0;
     input = new Scanner(System.in);
-    //Working block
     System.out.println("Enter the string");
     str = input.nextLine();
     System.out.println("Enter the search item");
     item = input.nextLine();
     item_len = item.length();
-    start = GetStart(str); //Get the starting point for the search
-    end = GetEnd(str); //Get the ending point for the search
+    start = GetStart(str); 
+    end = GetEnd(str); 
     i = start;
     while (i <= end){
       if (str.charAt(i) == item.charAt(j)){
@@ -40,36 +37,30 @@ public class Question_4{
       System.out.println("False");
   }
   public static int GetStart(String str){
-    //Declaration block
     int space, word, i;
-    //Initialization block
     i = 0;
     word = 0;
-    //Working block
     do{
       if (str.charAt(i) == ' ')
-        space = 1; //There is a space
+        space = 1; 
       else{
-        space = 0; //There is no space
-        word = 1; //At a word
+        space = 0; 
+        word = 1; 
       }
       i++;
     }while (word != 1 || space != 1);
     return i;
   }
   public static int GetEnd(String str){
-    //Declaration block
     int space, word, i;
-    //Initialization block
     i = str.length() - 1;
     word = 0;
-    //Working block
     do{
       if (str.charAt(i) == ' ')
-        space = 1; //There is a space
+        space = 1; 
       else{
-        space = 0; //There is no space
-        word = 1; //At a word
+        space = 0; 
+        word = 1; 
       }
       i--;
     }while (word != 1 || space != 1);
