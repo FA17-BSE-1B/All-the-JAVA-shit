d79cf104ec9c9b8acc6499d1b39ccfa5c59c0b7e
Elvin-Arrow <30791291+Elvin-Arrow@users.noreply.github.com>
2018-04-07 22:15:08 +0500

    Lab Sessional 1

--- Question_3.java
+++ Question_3.java
@@ -1,11 +1,9 @@
 import java.util.Scanner;
 public class Question_3{
   public static void main(String[] args){
-    //Declaration and Initialization block
     Scanner getInput = new Scanner(System.in); 
     int num; 
     int hex; 
-    //Working block
     System.out.print("Enter the number: ");
     num = getInput.nextInt();
     if (num >= 10 && num <= 15){
