45774e3af8e15174eb24cc8a0767322d621a4e9b
Elvin-Arrow <30791291+Elvin-Arrow@users.noreply.github.com>
2018-04-08 08:15:09 +0500

    5/04/18

---
+++ 2-D_Table.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class 2-D_Table{
+  public static void main(String[] args){
+    public static void main(String[] args){
+            for (int i = 0; i < 10; i++){
+                for (int j = 0; j < 10; j++)
+                    System.out.printf("%-4d", (j+1) * (i+1));
+                System.out.println();
+            }
+        }
+  }
+}
