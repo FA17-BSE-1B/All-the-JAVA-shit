94d5095a1dfb1a5a2fbae83337a069ee281e44d2
Elvin-Arrow <30791291+Elvin-Arrow@users.noreply.github.com>
2018-04-21 05:41:21 +0500

    19/4/18

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,13 +1,11 @@
 import java.util.Scanner;
 public class LinearSearch{
   public static void main(String[] args){
-    //Declaration block
     Scanner getInput = new Scanner(System.in);
     int [] array = new int[5];
     int searchItem;
     boolean flag = false;
     int len = array.length;
-    //Working block
     for (int i = 0; i < len; i++){
       System.out.print("Enter a number: ");
       array[i] = getInput.nextInt();
@@ -25,4 +23,3 @@ public class LinearSearch{
       System.out.println("Item not found");
   }
 }
-//System.out.println();
