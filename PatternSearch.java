4e98e4f4ce781e5ca37448e61b1ac6d6f8a2536d
Elvin-Arrow <30791291+Elvin-Arrow@users.noreply.github.com>
2018-04-21 20:58:21 +0500

    21/4/18

---
+++ PatternSearch.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class PatternSearch{
+  public static void main(String[] args) {
+        //Declaration block
+        boolean found;
+        String str, pattern;
+        int strLength, patternLegnth, position;
+        Scanner input = new Scanner(System.in);
+        //Working block
+        str = "a quick brown fox jumps over the lazy dog to fix it";
+        strLength = str.length();
+        System.out.print("Enter the search item: ");
+        pattern = input.nextLine();
+        patternLegnth = pattern.length();
+        for (int i = 0; i < strLength - patternLegnth; i++){
+            position = i;
+            found = true;
+            int j = 0;
+            while(j< patternLegnth && found){
+                if ((str.charAt(position) != pattern.charAt(j)) && pattern.charAt(j) != '?'){
+                    found = false;
+                }
+                position++;
+                j++;
+            }
+            if (found)
+                System.out.println("Pattern observed at " + i);
+        }
+    }
+}
