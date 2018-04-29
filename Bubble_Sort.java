8920832213d7ec584b41ee362f9e42b247307979
Elvin-Arrow <30791291+Elvin-Arrow@users.noreply.github.com>
2018-04-29 16:16:59 +0500

    12/04/18

--- Bubble_Sort.java
+++ Bubble_Sort.java
@@ -1,30 +1,24 @@
 public class Bubble_Sort{
   public static void main(String[] args){
-
-        //Declaration and initialization block
         int temp = 0;
-        int [] num = new int[5]; //Create a new array reference of type integer and size 5
-        int len = num.length-1; //Adjust the length varaible to tackle the array boundaries
-        boolean loopFlag; //flag for whether a swap was made or not during a particular iteration
-
-        //Working block
+        int [] num = new int[5]; 
+        int len = num.length-1;
+        boolean loopFlag; 
         for (int i = 0; i < 5; i++){
-            num[i] = (int)(Math.random() * 10); //Generate random numbers and store them in array num in integer form
+            num[i] = (int)(Math.random() * 10);
         }
         do{
-            loopFlag = false; //Initially assume that no swap is made
+            loopFlag = false; 
             for (int i = 0; i < len; i++){
                 if (num[i] > num[i+1]){
-                    temp = num[i]; //Save the value of index i in a temporary varaible
-                    num[i] = num[i+1]; //Replace the content of inde i with the contents of i + 1
-                    num[i+1] = temp; //Replace the contents of i + 1 with the contents of the temporary varaible
-                    loopFlag = true; //Swap was made
+                    temp = num[i];
+                    num[i] = num[i+1];
+                    num[i+1] = temp; 
+                    loopFlag = true; 
                 }
             }
             len--;
         }while(loopFlag);
-
-        //Display the array content
         for (int item: num){
             System.out.println(item);
         }
