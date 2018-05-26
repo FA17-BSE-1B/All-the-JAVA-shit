2645af9941954905b3f574bcd06eceaeccecd933
Elvin-Arrow <30791291+Elvin-Arrow@users.noreply.github.com>
2018-05-26 12:15:37 +0500

    Update Assignment3.java

--- Assignment3.java
+++ Assignment3.java
@@ -5,92 +5,64 @@ public class Assignment3 {
   public static void main(String[] args) throws Exception{
             Scanner getInput = new Scanner(System.in);
             File studentFile = new File("database.txt");
-            int userChoice = 1;
+            int usr = 1;
             while (userChoice != 0) {
-                System.out.println("Student Record Management");
                 System.out.println("1. Enter student data to the database");
                 System.out.println("2. Read student data from the database");
                 System.out.println("0. Exit program");
                 System.out.print("Your selection: ");
-                userChoice = getInput.nextInt();
-                switch (userChoice) {
+                usr = getInput.nextInt();
+                switch (usr) {
                     case 1:
                         writeData(studentFile, getInput);
                         break;
                     case 2:
-                        if (studentFile.exists())
-                            readData(studentFile, getInput);
-                        else
-                            System.out.println("File does not exists");
+                        readData(studentFile, getInput);
                         break;
                     case 0:
-                        System.out.println();
-                        System.out.println("Program Terminated");
                         break;
                     default:
                         System.out.println("Invalid selection");
                 }
-                System.out.println();
-                System.out.println("===============================================================");
-                System.out.println();
             }
         }
 
     public static void writeData(File studentFile, Scanner getInput) throws Exception{
-            //Declaration block
-            //Create object to write data to the file
             PrintWriter FileWriter = new PrintWriter(studentFile);
             int stdNum;
-            //Working block
-            System.out.println();
-            System.out.println("===============================================================");
-            System.out.println("Data Entry Portal");
-            System.out.println("===============================================================");
-            System.out.println();
             System.out.println("How many student do you want to enter data for?");
-            stdNum = getInput.nextInt(); //Recieve number of students
-            getInput.nextLine(); //Move the cursor to the next line
-            Student [] std = new Student[stdNum]; //Array to store student data
+            stdNum = getInput.nextInt(); 
+            getInput.nextLine(); 
+            Student [] std = new Student[stdNum]; 
             for (int i = 0; i < stdNum; i++){
-                //Initialise each array index to store data for each student
                 std[i] = new Student();
                 System.out.println("Student " + (i+1));
                 System.out.print("Name of the student: ");
-                std[i].Name = getInput.nextLine(); //Recieve the name of the student as string
+                std[i].Name = getInput.nextLine(); 
                 System.out.print("Roll number: ");
-                std[i].RollNo = getInput.nextLine(); //Recieve the roll number of the student as string
+                std[i].RollNo = getInput.nextLine(); 
                 System.out.print("Section: ");
-                std[i].Section = getInput.nextLine(); //Recieve the section of the student as string
+                std[i].Section = getInput.nextLine();
                 System.out.print("CGPA: ");
-                std[i].CGPA = getInput.nextFloat(); //Recieve the CGPA of the student as float
-                getInput.nextLine(); //Move the cursor to the next line
+                std[i].CGPA = getInput.nextFloat(); 
+                getInput.nextLine();
                 System.out.println("Record for " + std[i].Name + " successfully stored");
             }
-            //Store the data recieved in the array into the file
             for (int i = 0; i < stdNum; i++){
                 FileWriter.println(std[i].Name);
                 FileWriter.println(std[i].RollNo);
                 FileWriter.println(std[i].Section);
                 FileWriter.println(std[i].CGPA);
             }
-            FileWriter.close(); //Close the file
+            FileWriter.close();
         }
 
         public static void readData(File studentFile, Scanner getInput) throws Exception{
-            //Declaration block
             Scanner FileRead = new Scanner(studentFile);
             String searchName = "";
             int i, stdNum;
-            //Working block
-            stdNum = NumberOfRecords(studentFile); //Get the number of students in the file
+            stdNum = NumberOfRecords(studentFile); 
             getInput.nextLine();
-            System.out.println();
-            System.out.println("===============================================================");
-            System.out.println("Data Retrieval Portal");
-            System.out.println("===============================================================");
-            System.out.println();
-
-            //Retrieve all the data in file to an array
             Student[] std = new Student[stdNum];
             i = 0;
             while (FileRead.hasNext()) {
@@ -99,42 +71,35 @@ public class Assignment3 {
                 std[i].RollNo = FileRead.nextLine();
                 std[i].Section = FileRead.nextLine();
                 std[i].CGPA = FileRead.nextFloat();
-                FileRead.nextLine(); //Move the cursor to the next line
-                i++;//Next student
+                FileRead.nextLine(); 
+                i++;
             }
             while (true) {
-                System.out.println("Enter the name of the student to be searched\t\"Enter 0 to exit\"");
+                System.out.println("Enter the name of the student\t\"Enter 0 to exit\"");
                 searchName = getInput.nextLine();
                 if (searchName.equals("0"))
                     break;
                 for (i = 0; i < stdNum; i++) {
-                    //Check whether the student's details are in the file or not
                     if (searchName.equals(std[i].Name)) {
-                        //Display student details
                         System.out.println("Name: " + std[i].Name);
                         System.out.println("Roll Number: " + std[i].RollNo);
                         System.out.println("Section: " + std[i].Section);
                         System.out.println("CGPA: " + std[i].CGPA);
-                        System.out.println();
-                        break; //Terminated the search loop
+                        break;
                     }
                 }
-                if (i >= stdNum) //If student not found
+                if (i >= stdNum)
                     System.out.println("Student not found");
             }
         }
 
         public static int NumberOfRecords(File studentFile) throws Exception{
-            //Create Scanner object to read the data from file
             Scanner FileRead = new Scanner(studentFile);
             int lineCount = 0;
-            while(FileRead.hasNext()){ //while not the end of file
-                FileRead.nextLine(); //Go to next line
-                lineCount++; //Count the line
+            while(FileRead.hasNext()){ 
+                FileRead.nextLine(); 
+                lineCount++; 
             }
-            /*Each student has 4 attributes, so number of students is obtained
-              by dividing the number of lines by 4
-            */
             return (lineCount / 4);
         }
     }
